dc68c1e459f5bb6c6a1b5c408560d5151a6ba3fc
DanWilk <dcwilkinson@wilktech.com>
2021-09-19 12:38:36 -0400

    added for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,19 @@ var playerMoney = 10;
 //You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+ //   console.log(enemyNames[i]);
+ //   console.log(i);
+ //   console.log(enemyNames[i] + " is at " + i + " index");
+//}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
         window.alert("welcome to Robot Gladiators!");
 
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or SKIP to choose.");
@@ -73,4 +81,16 @@ var fight = function() {
 
 };
 
-fight()
+// fight()
+
+
+for(var i = 0; i < enemyNames.length; i++)
+    fight(enemyNames[i])
+//Game States
+//"WIN" - Player robot has defeated all enemy robots
+
+
+// * Fight all enemy-robots
+// * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
